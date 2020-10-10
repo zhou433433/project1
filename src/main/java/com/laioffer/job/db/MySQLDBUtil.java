3fed42f9ddf4c52054d6cf273eba2aecafd64011
zhou433433 <zhou433@wisc.edu>
2020-10-10 14:02:59 -0500

    my first commit

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -5,7 +5,7 @@ public class MySQLDBUtil {
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "laiproject";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = "Toefl110get!";
+    private static final String PASSWORD = "";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
